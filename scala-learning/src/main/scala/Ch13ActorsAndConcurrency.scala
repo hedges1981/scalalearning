15fbd3475495df3fd5a039c59bd17ed66d2aba07
rhall <rhall@sis.tv>
2017-02-21 10:00:37 +0000

    jjj

--- scala-learning/src/main/scala/Ch13ActorsAndConcurrency.scala
+++ scala-learning/src/main/scala/Ch13ActorsAndConcurrency.scala
@@ -1,6 +1,12 @@
+import akka.actor.Actor
+
 /**
   * Created by rhall on 20/02/2017.
   */
-class Ch13ActorsAndConcurrency {
+class Ch13ActorsAndConcurrency extends App{
+
+  class SimpleActor extends Actor {
+    override def receive: Receive = ???
+  }
 
 }
